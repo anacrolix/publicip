e0d16db1e41c46c33500e233c87391fc73f8527c
Matt Joiner <anacrolix@gmail.com>
2021-07-12 13:31:29 +1000

    Do lookups over HTTP too
    Needed when DNS lookups are restricted, such as for VPNs.

--- cmd/publicip/main.go
+++ cmd/publicip/main.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"context"
+	"time"
 
+	"github.com/alexflint/go-arg"
 	"github.com/anacrolix/publicip"
 	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
-	spew.Dump(publicip.GetAll(context.TODO()))
+	var args struct {
+		Ip4 bool `arg:"-4"`
+		Ip6 bool `arg:"-6"`
+	}
+	arg.MustParse(&args)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if args.Ip4 {
+		spew.Dump(publicip.Get4(ctx))
+	}
+	if args.Ip6 {
+		spew.Dump(publicip.Get6(ctx))
+	}
+	if !args.Ip4 && !args.Ip6 {
+		spew.Dump(publicip.GetAll(ctx))
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/anacrolix/publicip
 
 go 1.16
 
-require github.com/davecgh/go-spew v1.1.1
+require (
+	github.com/alexflint/go-arg v1.4.2 // indirect
+	github.com/davecgh/go-spew v1.1.1
+)
